b5e00ac76762928c0b59268aed4625db2af015eb
Joe Reich <jreich@covermymeds.com>
2016-11-24 22:34:03 -0500

    fixup initial commit

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,10 @@ Bundler.require(*Rails.groups)
 module BienResearchDatabase
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
   end
 end
