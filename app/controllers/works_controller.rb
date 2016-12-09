750608c954ab3a0125149e70bbe2dcd16df1d03e
Joe Reich <jreich@covermymeds.com>
2016-12-08 21:41:15 -0500

    add search functionality

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,5 @@
 class WorksController < ApplicationController
   def index
-    @works = Work.all
+    @works = Work.where('title LIKE ? OR keywords LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%")
   end
 end
