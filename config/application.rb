de8982c93abdd9827c4b44ab67c8f66a37e4dad7
Joe Reich <jreich@covermymeds.com>
2016-12-08 22:32:17 -0500

    upgrade to rails 5

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ Bundler.require(*Rails.groups)
 
 module BienResearchDatabase
   class Application < Rails::Application
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.generators do |g|
       g.test_framework :rspec
       g.integration_tool :rspec
