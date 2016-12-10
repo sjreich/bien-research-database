16c5c3c3c52263c6be1ac611a2b6a57b315d13bf
Joe Reich <jreich@covermymeds.com>
2016-12-10 17:06:39 -0500

    improve search with pg_search

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,27 @@
 class Work < ActiveRecord::Base
+  include PgSearch
+
   has_many :authorings, dependent: :destroy
   has_many :authors, through: :authorings
+
+  pg_search_scope :search,
+                  against: [
+                    :title,
+                    :journal,
+                    :keywords,
+                  ],
+                  associated_against: {
+                    authors: [
+                      :first_name,
+                      :last_name,
+                    ]
+                  },
+                  using: {
+                    tsearch: {
+                      prefix: true,
+                      dictionary: 'english',
+                    }
+                  }
 
   def author_string
     authors.map(&:to_s).join(', ')
