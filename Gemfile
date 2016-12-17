f0b8528ab80f9ade194ddb3d3df79fba0bae890b
Joe Reich <jreich@covermymeds.com>
2016-12-17 18:20:16 -0500

    add pagination

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'bootstrap-sass', '~> 3.3.7'
+gem 'will_paginate'
 
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,6 @@
 class WorksController < ApplicationController
   def index
     @works = Work.search(params[:search])
+                 .paginate(page: params[:page], per_page: 10)
   end
 end
