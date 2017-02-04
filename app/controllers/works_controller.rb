92f6c393416270fa0b52eda0e1d18209a8da2241
Joe Reich <jreich@covermymeds.com>
2017-02-04 18:21:52 -0500

    Filter search results by custom params

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,29 @@
 class WorksController < ApplicationController
+  before_action :prepare_search_options, only: [:index]
+
   def index
-    @works = Work.search(params[:search])
+    @works = Work.search(search_params)
                  .paginate(page: params[:page], per_page: 10)
+  end
+
+  private
+
+  def search_params
+    params.permit(
+      :full_search,
+      :title,
+      :author,
+      :keywords,
+      :published_after,
+      :published_before,
+      :language,
+      :type_of_work
+    ).select { |_, value| value.present? }
+  end
+
+  def prepare_search_options
+    @languages = Work.distinct.pluck(:language)
+    @types = Work.distinct.pluck(:type_of_work)
+    @authors = Author.order(:last_name).map { |author| [author.to_s, author.id] }
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -4,7 +4,14 @@ class Work < ActiveRecord::Base
   has_many :authorings, dependent: :destroy
   has_many :authors, through: :authorings
 
-  pg_search_scope :full_search,
+  PG_SEARCH_CONFIG = {
+    tsearch: {
+      prefix: true,
+      dictionary: 'english',
+    }
+  }.freeze
+
+  pg_search_scope :search_by_full_search,
                   against: [
                     :title,
                     :journal,
@@ -16,16 +23,21 @@ class Work < ActiveRecord::Base
                       :last_name,
                     ]
                   },
-                  using: {
-                    tsearch: {
-                      prefix: true,
-                      dictionary: 'english',
-                    }
-                  }
+                  using: PG_SEARCH_CONFIG
 
-  def self.search(query)
-    return all if query.empty?
-    full_search(query)
+  pg_search_scope :search_by_title, against: [:title], using: PG_SEARCH_CONFIG
+  pg_search_scope :search_by_keywords, against: [:keywords], using: PG_SEARCH_CONFIG
+
+  scope :search_by_language, -> (language) { where(language: language) }
+  scope :search_by_author, -> (author) { joins(:authors).where(authors: { id: author }).uniq }
+  scope :search_by_type_of_work, -> (type_of_work) { where(type_of_work: type_of_work) }
+  scope :search_by_published_after, -> (date) { where('publication_date > ?', date) }
+  scope :search_by_published_before, -> (date) { where('publication_date < ?', date) }
+
+  def self.search(params)
+    params.reduce(all) do |results, (param, value)|
+      results.send("search_by_#{param}", value)
+    end
   end
 
   def author_string
